812ec76f18beb5705c1652d882016ab174dad30c
William <mauritius.william.wijaya1@gmail.com>
2024-03-27 20:27:09 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# SOAL
+
+### SOAL 1 : Tampilkanlah seperti dibawah ini, dengan menggunakan loop
+
+```js
+1 2 3
+4 5
+6
+```
+
+   </br>
+   
+### SOAL 2 :  Tampilkanlah seperti dibawah ini, dengan menggunakan loop
+    * * * * *
+    *       *
+    *       *
+    *       *
+    * * * * *
+
+   </br>
+
+### SOAL 3 : cari angka prima antara 1 sampai 100 dengan loop
---
+++ index.js
@@ -0,0 +1,74 @@
+//? FOR LOOP
+// for (let i = 3; i > 0; i--) {
+//     console.log('hallo');
+// }
+
+
+//? WHILE LOOP
+// let random
+// let count = 0
+
+// while (random !== 5) {
+//     random = Math.ceil(Math.random() * 10)
+//     count++
+// }
+
+// console.log('perlu ' + count + ' putaran');
+
+//? NESTED LOOP
+// for (let i = 0; i < 5; i++) {
+//     for (let j = 0; j < 10; j++) {
+
+//     }
+// }
+
+
+//? BREAK & CONTINUE
+// BREAK akan force stop sebuah loop
+// CONTINUE akan skip code yang dibawahnya dan akan lanjut ke putaran selanjutnya
+for (let i = 0; i < 3; i++) {
+    break
+    console.log('hallo');
+    console.log('world');
+    console.log(i);
+}
+
+//? LATIHAN BUAT BINTANG
+// 1. print
+// * * *
+// * * *
+// * * *
+// for (let i = 3; i > 0; i--) {
+//     let result = ''
+//     for (let j = 0; j < 3; j++) {
+//         result += '* '
+//     }
+//     console.log(result);
+// }
+
+// 2. print
+// *
+// * *
+// * * *
+// for (let i = 0; i < 3; i++) {
+//     let result = ''
+//     for (let j = 0; j <= i; j++) {
+//         result += '* '
+//     }
+//     console.log(result);
+// }
+
+// 3. print
+// * * *
+// * *
+// *
+// for (let i = 3; i > 0; i--) {
+//     let result = ''
+//     for (let j = 0; j < i; j++) {
+//         result += '* '
+//     }
+//     console.log(result);
+// }
+
+
+
